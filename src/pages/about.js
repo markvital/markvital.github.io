7348d43d4bfb0db1ae371f567a3d08ecb2296c63
markvital <mark@fnf.vc>
2023-06-25 17:47:57 -0700

    Updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,8 +17,13 @@ const AboutPage = ({ data, location }) => {
             <h1>About Me</h1>
           </header>
           <p id="intro">
-            Full stack developer with an eye for user interfaces and a sense of usability. 
-            I have worked in large corporations for half of my career and in a startups later on. I feel like I have now combined both the professionalism of working on advanced products with the hacker mindset of a startup. I lean towards using open-sources frameworks and tools to achieve fast results.
+            <p>
+              I'm a full stack developer with an eye for user interfaces and a sense of usability. 
+              I have worked in large corporations for half of my career and in a startups later on. I feel like I have now combined both the professionalism of working on advanced products with the hacker mindset of a startup. I lean towards using open-sources frameworks and tools to achieve fast results.
+            </p>
+            <p>
+              Besides the software development my passion is an information design. Together with <a href="http://twitter.com/annavitals">@annavital</a> we created multiple <a href="https://blog.adioma.com">infographics</a> and a data visualization tool.
+            </p>
          </p>
           <StaticImage
             className="about-cover"
